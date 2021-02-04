5016ac1650598aa5abb1a94e59dd1032bd4cad08
James Scott Jr <skoona@gmail.com>
2021-02-03 21:42:29 -0500

    Added MetricsNode

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include <Arduino.h>
 #include "DHTNode.hpp"
 #include "RCWLNode.hpp"
+#include "MetricsNode.hpp"
 
 #ifdef ESP8266
 extern "C"
@@ -25,8 +26,8 @@ extern "C"
  * ex: sknSensors/deviceName/DHT_0/temperature -> 72.3 degress
  * Note: HomieNode(...range,lower,upper) manages this array suffix change; i.e no more name fixups
 */
-#define SKN_MOD_NAME "Environment-DHT-RCWL0516"
-#define SKN_MOD_VERSION "1.1.1"
+#define SKN_MOD_NAME "Monitor-DHT-RCWL-Metrics"
+#define SKN_MOD_VERSION "2.0.0"
 #define SKN_MOD_BRAND "SknSensors"
 
 #define SKN_TNODE_TITLE "Temperature and Humidity Sensor"
@@ -36,7 +37,11 @@ extern "C"
 #define SKN_MNODE_TYPE "sensor"
 
 #define SKN_TNODE_ID "Ambient"    
-#define SKN_MNODE_ID "Presence" 
+#define SKN_MNODE_ID "Presence"
+
+#define SKN_DNODE_TITLE "Device Info"
+#define SKN_DNODE_TYPE "sensor"
+#define SKN_DNODE_ID "hardware"
 
 // Select pins for Temperature and Motion
 #define PIN_DHT  D6                  // 12
@@ -45,11 +50,15 @@ extern "C"
 
 #define SENSOR_READ_INTERVAL 300
 
+ADC_MODE(ADC_VCC); //vcc read in MetricsNode
+
 HomieSetting<long> sensorsIntervalSetting("sensorInterval", "The interval in seconds to wait between sending commands.");
 HomieSetting<long> motionIntervalSetting("motionHoldInterval", "The interval in seconds to hold motion detection.");
+HomieSetting<long> broadcastInterval("broadcastInterval", "how frequently to send pin values.");
 
 DHTNode temperatureMonitor(PIN_DHT, DHT_TYPE, SKN_TNODE_ID, SKN_TNODE_TITLE, SKN_TNODE_TYPE, SENSOR_READ_INTERVAL);
 RCWLNode  motionMonitor(PIN_RCWL, SKN_MNODE_ID, SKN_MNODE_TITLE, SKN_MNODE_TYPE, SENSOR_READ_INTERVAL);
+MetricsNode metricsNode(SKN_DNODE_ID, SKN_DNODE_TITLE, SKN_DNODE_TYPE);
 
 bool broadcastHandler(const String &level, const String &value)
 {
@@ -72,9 +81,13 @@ void setup()
   motionIntervalSetting.setDefaultValue(60).setValidator([](long candidate) {
     return (candidate >= 10) && (candidate <= 300);
   });
+  broadcastInterval.setDefaultValue(60).setValidator([](long candidate) {
+    return (candidate >= 10) && (candidate <= 361);
+  });
 
   temperatureMonitor.setMeasurementInterval(sensorsIntervalSetting.get());
   motionMonitor.setMotionHoldInterval(motionIntervalSetting.get());
+  metricsNode.setMeasurementInterval(60);
 
   Homie_setFirmware(SKN_MOD_NAME, SKN_MOD_VERSION);
   Homie_setBrand(SKN_MOD_BRAND);
